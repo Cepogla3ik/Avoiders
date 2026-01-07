bec59842b61635d253715f2f810bf19eb3d1f260
Zirolio <zirolio@localhost.localdomain>
2026-01-08 00:14:06 +0200

    Ref

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,9 +1,1 @@
-import io from "socket.io-client";
-import { default as initGame } from "./game.js";
-import { default as initHome } from "./home.js";
-import "./styles.css";
-import "./default-styles.css";
-
-export const socket = io();
-initHome();
-initGame();
+console.log("TODO");
